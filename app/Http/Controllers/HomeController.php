34e819c2069becbe07705e3f65d14da8e97abbda
Alex <weberale@uni-hildesheim.de>
2020-11-14 20:23:35 +0100

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Data\Price\Prices_Day_Ahead;
 use App\Models\GraphData;
 use App\Charts\GraphDataChart;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use PhpParser\Node\Expr\Isset_;
 
 class HomeController extends Controller
 {
@@ -24,9 +27,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        $db = GraphData::pluck('price','date');
         $tempvar = file_get_contents("https://api.awattar.de/v1/marketdata");
         $tempvar = str_replace("}", "", $tempvar);
         $tempvar = explode ("{",$tempvar);
@@ -39,7 +41,43 @@ class HomeController extends Controller
         $chart = new GraphDataChart;
         $chart->labels($startVars);
         $chart->dataset('TestChart', 'line', $prices);
+        //For later use, this for test vaulues it will be fit to the test data
+        //$prices_day_ahead = Prices_Day_Ahead::where('Day', '=>', Carbon::yesterday()->format('Y-m-d'))
+        //    ->where('Day', '<', Carbon::tomorrow()->addDay()->format('Y-m-d'))->get();
+        //$tempDate = Carbon::yesterday();
+        $tempDate = '07.07.2020';
+        if($request->filled('date_example')) {
+            $tempDate = $request->date_example;
+        }
 
-        return view('home', ['chart' => $chart]);
+        $prices_day_ahead = Prices_Day_Ahead::where('Day', '>=', date('Y-m-d', strtotime('-1 days',strtotime($tempDate))))
+            ->where('Day', '<=', date('Y-m-d', strtotime('+1 days',strtotime($tempDate))))
+            ->orderBy('Day')
+            ->get();
+        $maxDateArray = [];
+
+        if (count($prices_day_ahead) == 3){
+            for ($i = 0; $i < 3; $i++) {
+                //$dateMax = $prices_day_ahead[$i]['Day'];
+                $hourMax = array_search($prices_day_ahead[$i]['Maximum'] ,$prices_day_ahead->toArray()[$i]);
+                //$prices_day_ahead[0]['Maximum']
+                if (Isset($hourMax) && strcmp($hourMax,'Maximum') != 0) {
+                    $hourMax = explode('_', $hourMax)[1];
+                    //3600 are seconds in a hour
+                    $hourMax = date('H:i', 3600 * $hourMax);
+                }
+                //$maxDateArray[] = $dateMax;
+                $maxDateArray[] = $hourMax;
+            }
+        }
+        //echo ($prices_day_ahead->toArray())[0];
+        if ($request->file('file')) {
+            $file = $request->file('file')->getRealPath();
+        }
+        echo 'sadfg';
+        echo  $request->file('file'). 'sdfhgdlfgjköl';
+        //Assume a hit every time, might need some counter measure to prevent null data
+        return view('home', ['chart' => $chart, 'dby'=>$prices_day_ahead[0]
+            , 'dbt'=>$prices_day_ahead[1],'dbtm'=>$prices_day_ahead[2], 'hY' => $maxDateArray, 'test' => 'Hour_1' ]);
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -13,3 +13,17 @@ if (!function_exists('return_if')) {
         }
     }
 }
+
+if (!function_exists('bg_color_prices')) {
+    function bg_color_prices($pricesDifference, $basePrice) {
+            $color = 'rgba(0,0,0,.8)';
+        if ($pricesDifference < 0) {
+            $percentage = abs(abs($pricesDifference) / ($basePrice == 0 ? 0.01 : $basePrice));
+            $color = 'rgba(255,0,0,'.$percentage.')';
+        } else {
+            $percentage = abs($pricesDifference / ($basePrice == 0 ? 0.01 : $basePrice));
+            $color = 'rgba(0, 255,0 ,'.$percentage.')';
+        }
+        return $color;
+    }
+}
