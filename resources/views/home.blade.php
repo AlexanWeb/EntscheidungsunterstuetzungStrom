d5cdce34d2c94fad173641be868fe22f55cff187
alayoubi <alayoubi.issam@gmail.com>
2020-11-23 21:24:14 +0100

    add 4 input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,103 +89,95 @@
         </div>
     </div>
 
-    @if(empty($prices_interady) > 0)
-        <div class="alert alert-warning">
-            <strong>Sorry!</strong> No Data for Interaday Found.
-        </div>
-    @else
+    <br>
+    <div class="card">
+        <h5 class="card-header">Angagen für Preise in MHw. Prices intera day</h5>
+        <div class="card-body">
+            <div class="container-fluid">
 
-        <br>
-        <div class="card">
-            <h5 class="card-header">Angagen für Preise in MHw. Prices intera day</h5>
-            <div class="card-body">
-                <div class="container-fluid">
-
-                    <div class="row">
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            Uhrzeit
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            {{$dby_pid['Day']}}
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            {{$dbt_pid['Day']}}
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            {{$dbtm_pid['Day']}}
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            {{$dbtum_pid['Day']}}
-                        </div>
+                <div class="row">
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        Uhrzeit
                     </div>
-
-                    <div class="row">
-                        {{-- rgba to change background color --}}
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            Maximum
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center">
-                            {{number_format($dby_pid['Maximum'], 2)}} € um {{$hY_pid[0]}}
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbt['Maximum'] - $dby['Maximum'], $dby['Maximum'])}}">
-                            {{number_format($dbt_pid['Maximum'], 2)}} € um {{$hY_pid[1]}}
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtm['Maximum'] - $dbt['Maximum'], $dbt['Maximum'])}}">
-                            {{number_format($dbtm_pid['Maximum'], 2)}} € um {{$hY_pid[2]}}
-                        </div>
-                        <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtm['Maximum'] - $dbt['Maximum'], $dbt['Maximum'])}}">
-                            {{number_format($dbtum_pid['Maximum'], 2)}} € um {{$hY_pid[3]}}
-                        </div>
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        {{$dby_pid['Day']}}
                     </div>
-
-
-
-
-                    @for($i = 1; $i < 25; $i++)
-
-                        <div class="row">
-                            @if($i===3)
-
-                                @for($j = 1; $j < 5; $j++)
-                                    <div class="col-sm-20 border-bottom border-top text-center"> {{date('H:i', (3600*($i-1)+ 60*($j-1)*15)) }} - {{date('H:i', (3600*($i-1)+ 60*($j)*15))}}</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center" > {{number_format($dby_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbt_pid['Hour_'.$i.'A_Q'.$j] - $dby_pid['Hour_'.$i.'A_Q'.$j], $dby_pid['Hour_'.$i.'A_Q'.$j])}}" > {{number_format($dbt_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center"
-                                         style="background-color: {{bg_color_prices($dbtm_pid['Hour_'.$i.'A_Q'.$j] - $dbt_pid['Hour_'.$i.'A_Q'.$j], $dbt_pid['Hour_'.$i.'A_Q'.$j])}}" >
-                                        {{number_format($dbtm_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
-                                    <div class="col-sm-20 border-bottom border-top text-center"
-                                         style="background-color: {{bg_color_prices($dbtum_pid['Hour_'.$i.'A_Q'.$j] - $dbtm_pid['Hour_'.$i.'A_Q'.$j], $dbtm_pid['Hour_'.$i.'A_Q'.$j])}}" >
-                                        {{number_format($dbtum_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
-                                @endfor
-
-
-                            @else
-
-                                @for($j = 1; $j < 5; $j++)
-                                    <div class="col-sm-20 border-bottom border-top text-center"> {{date('H:i', (3600*($i-1)+ 60*($j-1)*15)) }} - {{date('H:i', (3600*($i-1)+ 60*($j)*15))}}</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center" > {{number_format($dby_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbt_pid['Hour_'.$i.'_Q'.$j] - $dby_pid['Hour_'.$i.'_Q'.$j], $dby_pid['Hour_'.$i.'_Q'.$j])}}"> {{number_format($dbt_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtm_pid['Hour_'.$i.'_Q'.$j] - $dbt_pid['Hour_'.$i.'_Q'.$j], $dbt_pid['Hour_'.$i.'_Q'.$j])}}"> {{number_format($dbtm_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
-
-                                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtum_pid['Hour_'.$i.'_Q'.$j] - $dbtm_pid['Hour_'.$i.'_Q'.$j], $dbtm_pid['Hour_'.$i.'_Q'.$j])}}"> {{number_format($dbtum_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
-                                @endfor
-
-                            @endif
-
-                        </div>
-                    @endfor
-
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        {{$dbt_pid['Day']}}
+                    </div>
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        {{$dbtm_pid['Day']}}
+                    </div>
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        {{$dbtum_pid['Day']}}
+                    </div>
                 </div>
+
+                <div class="row">
+                    {{-- rgba to change background color --}}
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        Maximum
+                    </div>
+                    <div class="col-sm-20 border-bottom border-top text-center">
+                        {{number_format($dby_pid['Maximum'], 2)}} € um {{$hY_pid[0]}}
+                    </div>
+                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbt['Maximum'] - $dby['Maximum'], $dby['Maximum'])}}">
+                        {{number_format($dbt_pid['Maximum'], 2)}} € um {{$hY_pid[1]}}
+                    </div>
+                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtm['Maximum'] - $dbt['Maximum'], $dbt['Maximum'])}}">
+                        {{number_format($dbtm_pid['Maximum'], 2)}} € um {{$hY_pid[2]}}
+                    </div>
+                    <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtm['Maximum'] - $dbt['Maximum'], $dbt['Maximum'])}}">
+                        {{number_format($dbtum_pid['Maximum'], 2)}} € um {{$hY_pid[3]}}
+                    </div>
+                </div>
+
+
+
+
+                @for($i = 1; $i < 25; $i++)
+
+                    <div class="row">
+                        @if($i===3)
+
+                            @for($j = 1; $j < 5; $j++)
+                                <div class="col-sm-20 border-bottom border-top text-center"> {{date('H:i', (3600*($i-1)+ 60*($j-1)*15)) }} - {{date('H:i', (3600*($i-1)+ 60*($j)*15))}}</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center" > {{number_format($dby_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbt_pid['Hour_'.$i.'A_Q'.$j] - $dby_pid['Hour_'.$i.'A_Q'.$j], $dby_pid['Hour_'.$i.'A_Q'.$j])}}" > {{number_format($dbt_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center"
+                                     style="background-color: {{bg_color_prices($dbtm_pid['Hour_'.$i.'A_Q'.$j] - $dbt_pid['Hour_'.$i.'A_Q'.$j], $dbt_pid['Hour_'.$i.'A_Q'.$j])}}" >
+                                    {{number_format($dbtm_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
+                                <div class="col-sm-20 border-bottom border-top text-center"
+                                     style="background-color: {{bg_color_prices($dbtum_pid['Hour_'.$i.'A_Q'.$j] - $dbtm_pid['Hour_'.$i.'A_Q'.$j], $dbtm_pid['Hour_'.$i.'A_Q'.$j])}}" >
+                                    {{number_format($dbtum_pid['Hour_'.$i.'A_Q'.$j], 2)}} €</div>
+                            @endfor
+
+
+                        @else
+
+                            @for($j = 1; $j < 5; $j++)
+                                <div class="col-sm-20 border-bottom border-top text-center"> {{date('H:i', (3600*($i-1)+ 60*($j-1)*15)) }} - {{date('H:i', (3600*($i-1)+ 60*($j)*15))}}</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center" > {{number_format($dby_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbt_pid['Hour_'.$i.'_Q'.$j] - $dby_pid['Hour_'.$i.'_Q'.$j], $dby_pid['Hour_'.$i.'_Q'.$j])}}"> {{number_format($dbt_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtm_pid['Hour_'.$i.'_Q'.$j] - $dbt_pid['Hour_'.$i.'_Q'.$j], $dbt_pid['Hour_'.$i.'_Q'.$j])}}"> {{number_format($dbtm_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
+
+                                <div class="col-sm-20 border-bottom border-top text-center" style="background-color: {{bg_color_prices($dbtum_pid['Hour_'.$i.'_Q'.$j] - $dbtm_pid['Hour_'.$i.'_Q'.$j], $dbtm_pid['Hour_'.$i.'_Q'.$j])}}"> {{number_format($dbtum_pid['Hour_'.$i.'_Q'.$j], 2)}} €</div>
+                            @endfor
+
+                        @endif
+
+                    </div>
+                @endfor
+
             </div>
         </div>
-    @endif
-
+    </div>
 
 
 @endsection
