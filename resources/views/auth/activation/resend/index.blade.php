a6fe7a1a6bb85162693b0f3306d2e923f4db739a
Alex <weberale@uni-hildesheim.de>
2020-11-26 11:20:32 +0100

    Update

--- resources/views/auth/activation/resend/index.blade.php
+++ resources/views/auth/activation/resend/index.blade.php
@@ -4,14 +4,14 @@
     <div class="row">
         <div class="col-md8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Resend Stuff</div>
+                <div class="panel-heading">Resend activation email.</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{route('activation.resend.store')}}">
                         {{csrf_field()}}
 
                         <div class="form-group{{$errors->has('email') ? ' has-error' : ''}}">
-                            <label for="email" class="col-md-4 control-label"> E-mail Address</label>
-                            <div class="col-md-6">
+                            <label for="email" class="col-md-8 control-label"> E-mail Address</label>
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{old('email')}}">
                                 @if($errors->has('email'))
                                     <span class="help-block">
